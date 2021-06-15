aa4b8d9255b464196018b5d6ae137ee5e09c850e
Eduardo <xeduxbrux@gmail.com>
2021-06-15 15:09:28 -0300

    organizando codigo

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import{Cliente} from "./Cliente.js"
 import{ContaCorrente} from "./ContaCorrente.js"
 import { ContaPoupança } from "./ContaPoupança.js";
+import {Conta} from "./Conta.js"
 
 const cliente1 = new Cliente ("Ricardo",11122233309);
-const cliente2 = new Cliente ("Alicia", 88822233309);
 
-
-const contaCorrenteCliente1 = new ContaCorrente(cliente1,1001);
+const contaCorrenteCliente1 = new ContaCorrente(cliente1, 1001);
 contaCorrenteCliente1.depositar(500);
+contaCorrenteCliente1.sacar(100);
+
 
 const contaPoupança = new ContaPoupança (50, cliente1, 1001);
+
+
 console.log(contaPoupança);
-
-const conta2 = new ContaCorrente(cliente2, 102);
-contaCorrenteCliente1.transferir(200, conta2);
-
-console.log(ContaCorrente.numeroDeContas);
+console.log(contaCorrenteCliente1);
