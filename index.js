4855b6d927c91fde347eef91ac317cb6cc58eedb
Eduardo <xeduxbrux@gmail.com>
2021-06-17 18:08:08 -0300

    adicionando sistema de funcionarios e senhas

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 import{Cliente} from "./Cliente.js"
+import { Funcionario } from "./Funcionarios-Geral/Funcionario.js";
+import { Diretor } from "./Funcionarios-Geral/Diretor.js";
+import { Gerente } from "./Funcionarios-Geral/Gerente.js";
+import { SistemaDeAutenticacao } from "./SistemaDeAutenticacao.js";
 
-import{ContaCorrente} from "./ContaCorrente.js"
-import { ContaPoupança } from "./ContaPoupança.js";
-import {ContaSalario} from "./ContaSalario.js"
+const cliente = new Cliente ("Alicia", 9876543211, "456");
+const diretor = new Diretor("Rodrigo",10000, 12345678900 );
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente ("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha("122456");
 
 
-const cliente1 = new Cliente ("Ricardo",11122233309);
+const gerenteEstaLogado = SistemaDeAutenticacao.login(gerente, "122456");
+const diretorEstaLogado = SistemaDeAutenticacao.login(diretor, "123456");
+const clienteEstaLogado = SistemaDeAutenticacao.login(cliente, "46");
 
-const contaCorrenteCliente1 = new ContaCorrente(cliente1, 1001);
-contaCorrenteCliente1.depositar(500);
-contaCorrenteCliente1.sacar(100);
-
-
-const contaPoupança = new ContaPoupança (50, cliente1, 1001);
-contaPoupança.sacar(10);
-
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
-
-console.log(contaSalario);
-console.log(contaCorrenteCliente1);
-console.log(contaPoupança);
+console.log(clienteEstaLogado);
+console.log(gerenteEstaLogado);
+console.log(diretorEstaLogado);
